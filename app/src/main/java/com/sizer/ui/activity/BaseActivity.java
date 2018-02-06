87bd61aa964bb43f05a3b7b156c525e852d3b9f2
Stanislav Sover <soverstas@gmail.com>
2018-02-06 19:28:06 +0300

    update VideoActivity. change CameraView lib usage. move base logic to different class.

--- app/src/main/java/com/sizer/ui/activity/BaseActivity.java
+++ app/src/main/java/com/sizer/ui/activity/BaseActivity.java
@@ -1,7 +1,10 @@
 package com.sizer.ui.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
@@ -19,5 +22,13 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     public void showMessage(String msg) {
         Snackbar.make(findViewById(android.R.id.content), msg, Snackbar.LENGTH_SHORT).show();
+    }
+
+    public boolean isCameraPermissionGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public boolean isWriteExternalStoragePermissionGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
     }
 }
--- app/src/main/java/com/sizer/ui/activity/VideoActivity.java
+++ app/src/main/java/com/sizer/ui/activity/VideoActivity.java
@@ -3,39 +3,51 @@ package com.sizer.ui.activity;
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.devdoo.rxpermissions.RxPermission;
-import com.sizer.App;
+import com.otaliastudios.cameraview.CameraView;
 import com.sizer.R;
-import com.sizer.data.ILocalRepository;
-import com.sizer.model.ScanData;
-import com.sizer.ui.activity.video.SavePhotoTask;
-import com.wonderkiln.camerakit.CameraKit;
-import com.wonderkiln.camerakit.CameraKitEventCallback;
-import com.wonderkiln.camerakit.CameraKitImage;
-import com.wonderkiln.camerakit.CameraKitVideo;
-import com.wonderkiln.camerakit.CameraView;
+import com.sizer.ui.activity.video.LoopPhotoProcessor;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import butterknife.BindView;
 import rx.Subscription;
 
 public class VideoActivity extends BaseActivity {
 
-    private final int delayMillis = 2000;
+    private final int delayMillis = 1000;
+    private final int targetPhotoAmount = 60;
+
     @BindView(R.id.camera)
     CameraView cameraView;
-    List<SavePhotoTask> tasks = new ArrayList<>();
-    private static int frameCnt;
     Subscription subscription;
+    LoopPhotoProcessor loopPhotoProcessor = new LoopPhotoProcessor() {
 
+        @Override
+        protected boolean needTakeOneMorePhoto(int pictureAlreadyTakenAmount) {
+            return targetPhotoAmount > pictureAlreadyTakenAmount;
+        }
 
-    private static ILocalRepository localRepository;
+        @Override
+        protected CameraView getCameraView() {
+            return cameraView;
+        }
+
+        @Override
+        protected int getShutterPeriodInMills() {
+            return delayMillis;
+        }
+
+        @Override
+        protected void allPhotosProcessed() {
+            Log.d(LoopPhotoProcessor.TAG, "allPhotosProcessed");
+            startNextActivity();
+        }
+    };
+
 
     @Override
     int getContentViewLayoutResource() {
@@ -45,36 +57,49 @@ public class VideoActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        if (!isNeededPermissionsGranted()) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.READ_CONTACTS},
+//                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+//        }
+//
 
-        localRepository = App.getAppComponent().localDataRepository();
-        frameCnt = 0;
-
-        cameraView.setFacing(CameraKit.Constants.FACING_FRONT);
-        subscription =
-                RxPermission.with(getFragmentManager())
-                        .request(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
-                        .subscribe(granted -> {
-                            if (granted) {
-                                if (!cameraView.isStarted()) {
-//                                    cameraView.setVideoQuality(CameraKit.Constants.VIDEO_QUALITY_480P);
-                                    cameraView.setJpegQuality(100);
-                                    cameraView.setFocus(CameraKit.Constants.FOCUS_OFF);
-                                    cameraView.postDelayed(capturePreview, 1000);
-                                    //TODO: or capture captureVideo() call to capture video
-                                }
-                            }
-                        });
+        cameraView.addCameraListener(loopPhotoProcessor);
+        subscription = RxPermission.with(getFragmentManager())
+                .request(Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(granted -> {
+                    Log.d(LoopPhotoProcessor.TAG, "granted: " + granted);
+                    if (granted) {
+                        loopPhotoProcessor.startProcessPhoto();
+                    } else {
+                        VideoActivity.this.finish(); //go back
+                    }
+                });
 
     }
 
     @Override
     protected void onResume() {
+        Log.d(LoopPhotoProcessor.TAG, "onResume");
         super.onResume();
         cameraView.start();
+        subscription = RxPermission.with(getFragmentManager())
+                .request(Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(granted -> {
+                    Log.d(LoopPhotoProcessor.TAG, "granted: " + granted);
+                    if (granted) {
+                        loopPhotoProcessor.startProcessPhoto();
+                    } else {
+                        VideoActivity.this.finish(); //go back
+                    }
+                });
     }
 
     @Override
     protected void onPause() {
+        Log.d(LoopPhotoProcessor.TAG, "onPause");
         cameraView.stop();
         subscription.unsubscribe();
         super.onPause();
@@ -82,62 +107,22 @@ public class VideoActivity extends BaseActivity {
 
     @Override
     protected void onDestroy() {
-        for (SavePhotoTask task : tasks) {
-            if (!task.isCancelled()) {
-                task.cancel(false);
-            }
-        }
+        cameraView.destroy();
         super.onDestroy();
     }
 
-
-    private Runnable capturePreview = new Runnable() {
-        @Override
-        public void run() {
-            cameraView.captureImage(new CameraKitEventCallback<CameraKitImage>() {
-
-                @Override
-                public void callback(CameraKitImage cameraKitImage) {
-                    frameCnt++;
-                    SavePhotoTask task = new SavePhotoTask(localRepository, frameCnt);
-                    task.doInBackground(cameraKitImage);
-                    tasks.add(task);
-                }
-            });
-            if (frameCnt > 3) {
-                cameraView.stopVideo();
-                startActivity(new Intent(VideoActivity.this, RegisterActivity.class));
-                finish();
-            } else {
-                // Run again after approximately 1 second.
-                cameraView.postDelayed(this, 1000);
-            }
-        }
-    };
-
-    private void captureVideo() {
-        String str = localRepository.getUniqueDeviceId() + File.separator + localRepository.setScanData(new ScanData()).getScanId() + File.separator;
-        File scanPath = new File(Environment.getExternalStorageDirectory(), str);
-        File photo = new File(scanPath, "capture.mpeg");
-
-        scanPath.mkdirs();
-        cameraView.captureVideo(photo, new CameraKitEventCallback<CameraKitVideo>() {
-            @Override
-            public void callback(CameraKitVideo cameraKitVideo) {
-                startActivity(new Intent(VideoActivity.this, RegisterActivity.class));
-                finish();
-            }
-        });
-
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                cameraView.stopVideo();
-            }
-        }, delayMillis);
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(LoopPhotoProcessor.TAG, "onRequestPermissionsResult: " + Arrays.toString(permissions));
     }
 
+    private boolean isNeededPermissionsGranted() {
+        return isCameraPermissionGranted() && isWriteExternalStoragePermissionGranted();
+    }
 
+    private void startNextActivity() {
+        startActivity(new Intent(VideoActivity.this, RegisterActivity.class));
+        finish();
+    }
 }
